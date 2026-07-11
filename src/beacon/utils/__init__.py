feb70cb005170342b035ce973463385c406aefcd
ismitee <singhdivyam664@gmail.com>
2026-07-11 23:10:37 +0530

    BREAKING CHANGE: Completely discontinue and remove mod_check and prefix_mod_check decorators.

--- src/beacon/utils/__init__.py
+++ src/beacon/utils/__init__.py
@@ -1,4 +1,3 @@
-from .checks import mod_check, prefix_mod_check
 from .paginator import ViewPaginator, LayoutViewPaginator
 from .views import PrivateLayoutView, PrivateView, ConfirmationView, DestructiveConfirmationView
 from .timeparser import now_plus_seconds_unix, duration_to_seconds
