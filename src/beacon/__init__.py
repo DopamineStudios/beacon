0a12b411236af48e573294c49a0ec5d140b19c89
ismitee <singhdivyam664@gmail.com>
2026-07-15 21:26:52 +0530

    fix: Expose ViewPaginator through __init__.py (it got nuked before when i used tab autocomplete to add LayoutViewPaginator for some reason)

--- src/beacon/__init__.py
+++ src/beacon/__init__.py
@@ -1,3 +1,3 @@
 from .bot import BeaconBot, BeaconAutoShardedBot
-from .utils import now_plus_seconds_unix, duration_to_seconds, PrivateLayoutView, PrivateView, ConfirmationView, DestructiveConfirmationView, LayoutViewPaginator
+from .utils import now_plus_seconds_unix, duration_to_seconds, PrivateLayoutView, PrivateView, ConfirmationView, DestructiveConfirmationView, ViewPaginator, LayoutViewPaginator
 from .core import beacon_commands, preconditions
